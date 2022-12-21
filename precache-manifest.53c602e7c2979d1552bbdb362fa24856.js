654e85bc0fd3a352d0b1c295c486a9e6b69054e9
Goglikooo <goglikooo@gmail.com>
2022-12-22 02:47:05 +0400

    Updates

---
+++ precache-manifest.53c602e7c2979d1552bbdb362fa24856.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e9d61e5491667a5020",
+    "url": "/git-user-search/static/css/main.a6efa1d1.chunk.css"
+  },
+  {
+    "revision": "f5e9d61e5491667a5020",
+    "url": "/git-user-search/static/js/main.f5e9d61e.chunk.js"
+  },
+  {
+    "revision": "d3a3de06147fdeb4100b",
+    "url": "/git-user-search/static/js/1.d3a3de06.chunk.js"
+  },
+  {
+    "revision": "aeaa776c169ed4609d57",
+    "url": "/git-user-search/static/js/runtime~main.aeaa776c.js"
+  },
+  {
+    "revision": "b433e22d01073904c774a3aab11bfab0",
+    "url": "/git-user-search/static/media/icon-location.b433e22d.svg"
+  },
+  {
+    "revision": "547abbf4352e547b4f80658c370b6c43",
+    "url": "/git-user-search/static/media/icon-website.547abbf4.svg"
+  },
+  {
+    "revision": "4e1e65c47abe57b0fdfb19144089d7d9",
+    "url": "/git-user-search/static/media/icon-twitter.4e1e65c4.svg"
+  },
+  {
+    "revision": "64e9c16d84797ff56142e84957b6a212",
+    "url": "/git-user-search/static/media/icon-company.64e9c16d.svg"
+  },
+  {
+    "revision": "d4dcb16294ad206b5627a65ed7f5fff4",
+    "url": "/git-user-search/static/media/icon-search.d4dcb162.svg"
+  },
+  {
+    "revision": "062faca2a09f52936088587dae9f6de1",
+    "url": "/git-user-search/static/media/icon-moon.062faca2.svg"
+  },
+  {
+    "revision": "d8ea436b290b055fa9382cc42213b2d7",
+    "url": "/git-user-search/static/media/icon-sun.d8ea436b.svg"
+  },
+  {
+    "revision": "f6b245c5d180ec2feaa6acc6dcf1eaf1",
+    "url": "/git-user-search/index.html"
+  }
+];
